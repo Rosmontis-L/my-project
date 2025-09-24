aa2e13b5b8641ab50ef6bce7f7afebf52888e05e
Rosmontis <18269736785@163.com>
2025-09-24 18:07:28 +0800

    基本页面配置

---
+++ my-project-frontend/src/views/WelcomeView.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+
+<template>
+  <div style="width: 100vw; height: 100vh; overflow: hidden; display: flex">
+    <div style="flex: 1;background-color: black">
+      <el-image style="width: 100%;height: 100%" fit="cover"
+          src="https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297_960_720.jpg"/>
+    </div>
+    <div class="welcome-title">
+      <div style="font-size: 30px;font-weight: bold">欢迎来到我们的学习平台</div>
+      <div style="margin-top: 10px">在这里可以学习如何使用Java以及搭建简单的网站</div>
+      <div style="margin-top: 5px">在这里你可以与其它用户相互交流学习经验</div>
+    </div>
+    <div class="right-card">
+
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.welcome-title {
+  position: absolute;
+  bottom: 30px;
+  left: 30px;
+  color: white;
+  text-shadow: 0 0 10px black;
+}
+.right-card {
+  width: 400px;
+  z-index: 1;
+  background-color: white;
+}
+</style>
